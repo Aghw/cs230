18b5a12978c45b5c3df78c63d5cc962bd5780b17
Aghw <sam.asmerom@outlook.com>
2018-04-15 02:46:32 -0700

    completed homework1 and ready to submit

---
+++ tree/master/Homework1/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult BirthdayCard()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult BirthdayCard(Models.BirthdayCard birthday)
+        {
+            //return View();
+            if (ModelState.IsValid)
+            {
+                return View("Happy", birthday);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ tree/master/Homework1/BirthdayCard/Models/BirthdayCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter the From name")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter the To name")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter a birthday message!")]
+        public string Message { get; set; }
+    }
+}
